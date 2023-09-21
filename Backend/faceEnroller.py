6f1c89bded6c5d45c3161c5b7c898ed85082a4fa
Akhilesh Iyer <116811351+akhilesh-iyer@users.noreply.github.com>
2023-09-21 20:05:57 +0530

    Add files via upload

---
+++ Backend/faceEnroller.py
@@ -0,0 +1,32 @@
+# Import libraries
+import cv2
+
+# Assigning paths
+file_path = 'C:/Users/Mi Notebook/Desktop/facedata/Train/'
+face_cascade = cv2.CascadeClassifier('C:/Users/Mi Notebook/Downloads/haarcascade_frontalface_alt2.xml')
+
+cam = cv2.VideoCapture(0) # Creating camera object
+
+# Storing captured photos
+while True:
+    ret, frame = cam.read()
+    frame = cv2.flip(frame, 1)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Convert to grayscale
+    face = face_cascade.detectMultiScale(frame, 1.1, 4) # Detect face
+    cv2.imshow("window", frame)
+
+    k = cv2.waitKey(1)
+    if k == ord('q'): # ord() gives unicode of letter
+        print("Exited successfully!")
+        break
+    elif k == ord(' '):
+        username = input("Enter student's moodle ID: ")
+        # Crop face
+        for (x, y, w, h) in face:
+            face = frame[y:y + h, x:x + w] 
+        face = cv2.equalizeHist(face) # Equalize histogram for improving accuracy
+        face = cv2.resize(face, (640, 640))
+        cv2.imwrite(file_path + username + '.jpg', face)
+        print('Image saved successfully!')
+cam.release()
+cv2.destroyAllWindows()
